caaef0ecd2cd7fce10615dddd15206f2fcc897fc
Greta Kaminskaite <greta.kam@gmail.com>
2022-06-13 18:37:33 +0300

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<ProductModel> productsList;
+        public ProductController()
+        {
+            productsList = DataBase.ProductsList;
+        }
+        public IActionResult Index()
+        {
+            productsList.Sort((x, y) => y.Price.CompareTo(x.Price));
+            return View(productsList);
+        }
+
+        public IActionResult ProductView(string name)
+        {
+            var product = productsList.Find(product => product.Name == name);
+
+            return View(product);
+        }
+
+        [HttpGet]
+        public IActionResult CreateForm()
+        {
+            ProductFormModel newProduct = new ProductFormModel();
+            newProduct.CategoriesList = DataBase.CategoriesList;
+            return View(newProduct);
+        }
+
+        [HttpPost]
+        public IActionResult CreateForm(ProductFormModel newProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductModel product = new ProductModel
+                {
+                    Name = newProduct.Name,
+                    Price = newProduct.Price,
+                    Description = newProduct.Description,
+                    CategoryName = newProduct.CategoryName
+                };
+                DataBase.ProductsList.Add(product);
+                return RedirectToAction("Index");
+
+            }
+            newProduct.CategoriesList = DataBase.CategoriesList;
+            return View(newProduct);
+        }
+    }
+}
---
+++ DataBase.cs
@@ -0,0 +1,22 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public static class DataBase
+    {
+        
+        public static List<CategoriesModel> CategoriesList = new List<CategoriesModel> {
+             new CategoriesModel { ID = 1, Name = "Computers" },
+             new CategoriesModel{ID = 2, Name = "Smart phones"},
+             new CategoriesModel { ID = 3, Name = "Tablets" }
+        };
+
+
+        public static List<ProductModel> ProductsList = new List<ProductModel>
+            {
+                new ProductModel{Name = "HP", CategoryName = "Computers", Price = 987.5},
+                new ProductModel{Name =  "Asus", CategoryName = "Computers", Price = 885.99},
+                new ProductModel { Name = "Huawei p30", CategoryName = "Smart phones", Price = 335.76 }
+            };
+    }
+}
